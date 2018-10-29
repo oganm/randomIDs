1b24bafb614fc7d8af907ec93763cb2b73c67905
OganM <ogan.mancarcii@gmail.com>
2018-10-29 14:21:59 -0700

    data-raw and processing

---
+++ data-raw/process.R
@@ -0,0 +1,23 @@
+library(here)
+library(glue)
+devtools::load_all()
+setwd(here())
+
+namelists = list.files('data-raw/',full.names = TRUE)
+
+namelists = namelists[!grepl('process\\.R',namelists)]
+
+for (x in namelists){
+    listname = basename(x)
+    files = list.files(namelists,full.names = TRUE)
+
+    names = stringr::str_extract(files,'(?<=_).*')
+
+    teval(glue('{listname}<<-list()'))
+
+    for(i in seq_along(files)){
+        words = readLines(files[i])
+        teval(glue('{listname}${names[i]}<<-words'))
+    }
+    teval(glue('devtools::use_data({listname},overwrite = TRUE)'))
+}
